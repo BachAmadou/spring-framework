eba7963dd7374b46ca8e92d81a21be7f24dd2521
Bachir <bafosth@yahoo.fr>
2023-08-29 14:55:21 -0400

    created DI module, classes and config

---
+++ Spring-03-Core-DI/src/main/java/com/dev/RunApp.java
@@ -0,0 +1,15 @@
+package com.dev;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class RunApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+        Java java = container.getBean(Java.class);
+
+        java.getTeachingHours();
+    }
+}
