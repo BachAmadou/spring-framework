ed44b021142f3a7b3c1de5409d27fea9bf104658
Bachir <bafosth@yahoo.fr>
2023-08-31 13:47:13 -0400

    Added some implementation on the classes

--- Spring-04-Abstraction/src/main/java/com/dev/repository/DBCommentRepository.java
+++ Spring-04-Abstraction/src/main/java/com/dev/repository/DBCommentRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
 public class DBCommentRepository implements CommentRepository{
     @Override
     public void storeComment(Comment comment) {
-        System.out.printf("Storing comment: " + comment.getText());
+        System.out.println("Storing comment: " + comment.getText());
     }
 }
--- Spring-04-Abstraction/src/main/java/com/dev/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/dev/service/CommentService.java
@@ -3,6 +3,7 @@ package com.dev.service;
 import com.dev.model.Comment;
 import com.dev.proxy.CommentNotificationProxy;
 import com.dev.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
