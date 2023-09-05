d3733d486a5ee47b90ee25920b59c5f02147a3c1
Bachir <bafosth@yahoo.fr>
2023-09-05 14:33:01 -0400

    Created html files (home, about, userinfo) and controller class

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/dev/ControllerApplication.java
@@ -0,0 +1,23 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ControllerApplication.class, args);
+	}
+
+//	@SpringBootApplication
+//	public static class ControllerApplication {
+//
+//		public static void main(String[] args) {
+//
+//			SpringApplication.run(ControllerApplication.class, args);
+//		}
+//
+//	}
+}
