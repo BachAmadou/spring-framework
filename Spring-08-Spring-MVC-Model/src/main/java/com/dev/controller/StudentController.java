a9b7e3d17564633c1bb06368a933f97a0fb3eec2
Bachir <bafosth@yahoo.fr>
2023-09-06 20:54:39 -0400

    Added to the controller class different data manipulations such as String, int, List.

--- Spring-08-Spring-MVC-Model/src/main/java/com/dev/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/dev/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +16,19 @@ public class StudentController {
 
         model.addAttribute("name", "Java course");
         model.addAttribute("course", "MVC");
+
+        String subject = "Collections, API";
+        model.addAttribute("subject", subject);
+
+        int studentID = new Random().nextInt(100) ;
+        model.addAttribute("studentID", studentID);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        model.addAttribute("numbers", numbers);
+
 
         return "student/welcome";
     }
