2678f6be4e78811646d3ca049561dfe2c03525cf
Bachir <bafosth@yahoo.fr>
2023-09-09 20:27:11 -0400

    Created the bank simulation project and added the necessary classes, and interfaces

---
+++ bank-simulation-app/src/main/java/com/dev/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package com.dev.repository;
+
+import com.dev.model.Account;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AccountRepository {
+
+    public static List<Account> accountList = new ArrayList<>();
+
+    public Account save(Account account) {
+        accountList.add(account);
+        return account;
+    }
+}
---
+++ bank-simulation-app/src/main/java/com/dev/service/AccountServiceImpl.java
@@ -0,0 +1,37 @@
+package com.dev.service;
+
+import com.dev.enums.AccountType;
+import com.dev.model.Account;
+import com.dev.repository.AccountRepository;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class AccountServiceImpl implements AccountService{
+
+    AccountRepository accountRepository;
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Account createNewAccount(BigDecimal balance, Date createDate, AccountType accountType, Long userId) {
+        // create account object
+        Account account  = Account.builder().id(UUID.randomUUID()).userId(userId)
+                .balance(balance).accountType(accountType).creationDate(createDate).build();
+
+        // save into the database(repository)
+
+
+        // return the object created
+
+        return null;
+    }
+
+    @Override
+    public List<Account> listAllAccount() {
+        return null;
+    }
+}
