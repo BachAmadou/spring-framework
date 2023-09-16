fe7083a034d4d397695954ef7b19768f11b13d6d
Bachir <bafosth@yahoo.fr>
2023-09-16 12:58:39 -0400

    added diffent logic to transactionServiceImpl, created exception error messages

--- bank-simulation-app/src/main/java/com/dev/repository/AccountRepository.java
+++ bank-simulation-app/src/main/java/com/dev/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package com.dev.repository;
 
+import com.dev.exception.RecordNotFoundException;
 import com.dev.model.Account;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class AccountRepository {
@@ -19,4 +21,36 @@ public class AccountRepository {
     public List<Account> findAll() {
         return accountList;
     }
+
+    public Account findById(UUID id) {
+        // complete the method that finds the account inside the list, if not throw RecordNotFoundException
+        return accountList.stream()
+                .filter(account -> account.getId().equals(id))
+                .findAny().orElseThrow(() -> new RecordNotFoundException("Account does not exist in the database"));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
