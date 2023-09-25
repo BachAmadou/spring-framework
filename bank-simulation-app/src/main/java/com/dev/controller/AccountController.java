b8320bee1dbf0fd638dc86ee878f6bc94887b758
Bachir <bafosth@yahoo.fr>
2023-09-25 15:02:58 -0400

    added controller class to account

---
+++ bank-simulation-app/src/main/java/com/dev/controller/AccountController.java
@@ -0,0 +1,25 @@
+package com.dev.controller;
+
+import com.dev.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/index")
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("accountList", accountService.listAllAccount());
+
+        return "account/index";
+    }
+}
